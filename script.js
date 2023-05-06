ea6485b14e362457a36ed0a8a5a07dfb21517fa3
Kaitlin Leung <leungkn@uci.edu>
2023-05-06 15:21:34 -0700

    cat dictionaries
    Co-authored by: Trisha Tong trishatong2043@gmail.com
    Co-authored by: Paris Haraguchi pharaguc@uci.edu

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-{
-    let energy_level = undefined;
-    let grooming = undefined;
-    let social_needs = undefined;
-    let indoor = undefined;
-    let intelligence = undefined;
+let energy_level = undefined;
+let grooming = undefined;
+let social_needs = undefined;
+let indoor = undefined;
+let intelligence = undefined;
+let catPoints = 
+{ 
+    cspa: 0,
+    bsho: 0,
+    aege: 0,
+    bamb: 0,
+    chee: 0,
+    lihu: 0
+};
+
+let catInfo = { // if indoor == 0, it's an outdoor cat
+    cspa: [5, 1, 3, 0, 0],
+    bsho: [2, 2, 3, 0, 3],
+    aege: [3, 3, 4, 0, 3],
+    bamb: [5, 1, 3, 0, 5],
+    chee: [4, 1, 3, 0, 5],
+    lihu: [3, 1, 4, 1, 3]
 }
 
 function changeEnergy(value) {
     energy_level = value;
-    console.log(energy_level);
 }
 
 function changeGrooming(value) {
@@ -27,5 +42,17 @@ function changeIntelligence(value) {
     intelligence = value;
 }
 
+function allChecked() { // returns true if all are checked
+    return energy_level !== undefined && grooming !== undefined && social_needs !== undefined && indoor !== undefined && intelligence !== undefined;
+}
 
+function determineCat() {
+    if (allChecked()) {
+        // increase counts and choosing a cat (tie breaking if necessary)
+        console.log("I have all answers");
 
+    }
+    else {
+        alert("Please answer all the questions");
+    }
+}
