ccb954169775cdbe932568b34f2a6fdce2d2da87
Kaitlin Leung <leungkn@uci.edu>
2023-05-06 14:41:06 -0700

    Add onclick functions
    Co-authored by: Trisha Tong trishatong2043@gmail.com
    Co-authored by: Paris Haraguchi pharaguc@uci.edu

--- script.js
+++ script.js
@@ -6,3 +6,26 @@
     let intelligence = undefined;
 }
 
+function changeEnergy(value) {
+    energy_level = value;
+    console.log(energy_level);
+}
+
+function changeGrooming(value) {
+    grooming = value;
+}
+
+function changeSocialNeeds(value) {
+    social_needs = value;
+}
+
+function changeIndoor(value) {
+    indoor = value;
+}
+
+function changeIntelligence(value) {
+    intelligence = value;
+}
+
+
+
