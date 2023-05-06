1494d97677258a231f574397575d661f0ff0e32a
Kaitlin Leung <leungkn@uci.edu>
2023-05-06 16:50:07 -0700

    lalala changes api call added and max
    Co-authored by: Trisha Tong trishatong2043@gmail.com
    Co-authored by: Paris Haraguchi pharaguc@uci.edu

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ let grooming = undefined;
 let social_needs = undefined;
 let indoor = undefined;
 let intelligence = undefined;
+let APIkey = undefined;
+const BREEDS_BASE_URL = "https://api.thecatapi.com/v1/breeds";
+const IMAGES_BASE_URL =  "https://api.thecatapi.com/v1/images/search" //?limit=10&breed_ids=beng&api_key=REPLACE_ME"
+var fs = require('fs');
+
 let catPoints = 
 { 
     cspa: 0,
@@ -34,7 +39,10 @@ function changeSocialNeeds(value) {
     social_needs = value;
 }
 
-function changeIndoor(value) {
+function changeIndoor(value, event) {
+    console.log(event);
+    //document.getElementsByClassName('questinoons).fhilcegernb, clear all classes if want to have de-select
+    event.target.className = "selected";
     indoor = value;
 }
 
@@ -68,7 +76,57 @@ function setCatPoints() {
     }
 }
 
+function maxCat() {
+    let bestCat = undefined;
+    let maxPoints = 0;
+    Object.keys(catPoints).forEach((key) => {
+        if (catPoints[key] >= maxPoints) {
+            bestCat = key;
+            maxPoints = catPoints[key];
+        }
+    });
+    return bestCat;
+}
+
+function makeImageRequest(breed) {
+    var headers =  {
+        "Content-Type": "application/json",
+        "x-api-key": APIkey,
+    };
+}
+
+function makeBreedRequest(breed) {
+    var headers =  {
+        "Content-Type": "application/json",
+    };
+
+    fetch(BREEDS_BASE_URL + "/" + breed, {method: 'GET', headers: headers}).then(response => {
+        console.log(url);
+        if(response.ok) {
+            return response.json();
+        }
+    });
+}
+
 function getCat() {
+    loadAPI();
     setCatPoints();
     // determine which cat is highest in catPoints -> API call
+    let finalCat = maxCat();
+    breedReturn = makeBreedRequest(finalCat);
+    console.log(breedReturn);
+    //imagesReturn = makeImageRequest(finalCat);
 }
+
+function loadAPI() {
+    //This chains two promises together. First, client_secret.json will be read and parsed. Once it completes, tokens.json will be read and parsed.
+    //Promise.all() could be used to conduct these two file reads asynchronously, which is more efficient.
+    fs.readFile('APIKey.json', (err, data) => {
+        if(err){
+            console.log(err + "\n\nHave you created your tokens and client_secret files yet?")
+        }else{
+            data = JSON.parse(data);
+            APIkey = data.api_key;
+        }
+    })
+} 
